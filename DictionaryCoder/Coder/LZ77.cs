19491a5a26ebed9fbe3b1a61edd9d876981a4bb1
Raff <56380303+Raff-dev@users.noreply.github.com>
2020-07-07 18:30:12 +0200

    initial commit

---
+++ DictionaryCoder/Coder/LZ77.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace DictionaryCoder.Coder
+{
+
+    public class LZ77
+    {
+        static int windowSize = 128;
+        static internal int bufferSize = 128;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello werld");
+        }
+
+        public void GetInput()
+        {
+            throw new NotImplementedException("function not implemented");
+        }
+
+        public void WriteOutput()
+        {
+            throw new NotImplementedException("function not implemented");
+        }
+
+        public void Decode()
+        {
+            throw new NotImplementedException("function not implemented");
+
+        }
+        public void Encode()
+        {
+            throw new NotImplementedException("function not implemented");
+
+        }
+    }
+}
