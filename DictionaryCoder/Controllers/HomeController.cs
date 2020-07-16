f50ea653f86fc1dfb940cf1e7d69eaf76aa87412
Raff <56380303+Raff-dev@users.noreply.github.com>
2020-07-16 20:57:15 +0200

    drag and drop styling

--- DictionaryCoder/Controllers/HomeController.cs
+++ DictionaryCoder/Controllers/HomeController.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using DictionaryCoder.Models;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Text;
 
 namespace DictionaryCoder.Controllers
 {
@@ -36,6 +38,34 @@ namespace DictionaryCoder.Controllers
         public IActionResult Sources()
         {
             return View();
+        }
+
+        [HttpPost("api/upload")]
+        public void ProcessRequest(HttpContext context)
+        {
+            if (context.Request.Form.Files.Count > 0)
+            {
+                IFormFileCollection files = context.Request.Form.Files;
+                foreach (IFormFile file in files)
+                {
+                    using (System.IO.Stream fsSource = file.OpenReadStream())
+                    {
+                        using (StreamWriter outputFile = new StreamWriter(Path.Combine("kek", ".txt")))
+                        {
+                            while (true)
+                            {
+                                byte[] arr = new byte[fsSource.Length];
+                                int bytesToRead = (int)fsSource.Length;
+                                if (fsSource.Read(arr, 0, bytesToRead) <= 0) break;
+                                foreach (var content in arr) Console.Write((char)content);
+                                //run coder here (outputFile/fssource)
+                            }
+                        }
+                    }
+                }
+            }
+            context.Response.ContentType = "text/plain";
+            context.Response.WriteAsync("File(s) uploaded successfully!");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
