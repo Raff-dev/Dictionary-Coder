13f26a8637f9da4ab8aef59dddbf0449ba196c2b
Raff <56380303+Raff-dev@users.noreply.github.com>
2020-07-16 20:56:32 +0200

    drag and drop file detection

--- DictionaryCoder/wwwroot/js/site.js
+++ DictionaryCoder/wwwroot/js/site.js
@@ -1,4 +1,53 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var selectedFiles;
+var validEncodeExtentions = [".txt", ".jpg", ".jpeg", ".png"]
+var validDecodeExtentions = [".lz77"]
 
-// Write your JavaScript code.
+function OnDragEnter(e) {
+    e.stopPropagation();
+    e.preventDefault();
+}
+
+function OnDragOver(e) {
+    e.stopPropagation();
+    e.preventDefault();
+}
+
+function OnDrop(e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    selectedFiles = e.dataTransfer.files;
+
+    file = selectedFiles[0];
+    console.log('name: ' + file.name);
+}
+
+$(document).ready(function () {
+    var file;
+    file = document.getElementById("encode-drag-file");
+    file.addEventListener("dragenter", OnDragEnter, false);
+    file.addEventListener("dragover", OnDragOver, false);
+    file.addEventListener("drop", OnDrop, false);
+});
+
+$("#encode-button").click(function () {
+    var data = new FormData();
+    for (var i = 0; i < selectedFiles.length; i++) {
+        data.append(selectedFiles[i].name, selectedFiles[i]);
+    }
+    $.ajax({
+        type: "POST",
+        url: "FileHandler.ashx",
+        contentType: false,
+        processData: false,
+        data: data,
+        success: function (result) {
+            alert(result);
+        },
+        error: function () {
+            alert("There was error encode-buttoning files!");
+        }
+    });
+});
+
+console.log("koniaaa")
