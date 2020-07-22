90d9b2b9d7bd5f3ada528ad09c1778e02479203d
Raff <56380303+Raff-dev@users.noreply.github.com>
2020-07-22 13:46:58 +0200

    Improved documentation and console crogress bar

--- DictionaryCoder/Coder/Coder.cs
+++ DictionaryCoder/Coder/Coder.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using Coder;
 
 namespace DictionaryCoder.Coder
 {
@@ -16,10 +17,12 @@ namespace DictionaryCoder.Coder
         int LengthBytesCount;
         int SizeSum;
 
-        public Coder(int CodeBufferSize, int LookAheadBufferSize)
+        public Coder(int CodeBufferSize = 4095, int LookAheadBufferSize = 15)
         {
             this.CodeBufferSize = CodeBufferSize;
             this.LookAheadBufferSize = LookAheadBufferSize;
+
+            // Calculation of count of bytes needed to write down given numbers.
             OffsetBytesCount = (int)Math.Floor(Math.Log10(CodeBufferSize + LookAheadBufferSize) / Math.Log10(256) + 1);
             LengthBytesCount = (int)Math.Floor(Math.Log10(LookAheadBufferSize) / Math.Log10(256) + 1);
             SizeSum = 3;
@@ -40,47 +43,67 @@ namespace DictionaryCoder.Coder
         * Parameters : inputStream - file stream from witch to read and compress data from.
         *              outputPath - path to where create and write encoded data to
         */
-        public void EncodeLZ77(Stream inputStream, string outputPath)
+        public void EncodeLZ77(string inputFileName, string outputFileName)
         {
-            byte[] codeBuffer = new byte[CodeBufferSize];
-            byte[] lookAheadBuffer = new byte[LookAheadBufferSize + 1];
-
-            int shift = 0;
-            int bytesToRead = 0;
-            int bytesEncoded = 0;
-            int inputSize = -1;
-
-            using (Stream outputStream = File.OpenWrite(outputPath))
+            using (Stream inputStream = new FileStream(inputFileName, FileMode.Open, FileAccess.Read))
             {
-                BinaryWriter binaryWriter = new BinaryWriter(outputStream);
+                byte[] codeBuffer = new byte[CodeBufferSize];
+                byte[] lookAheadBuffer = new byte[LookAheadBufferSize + 1];
 
-                while (LookAheadBufferSize > 0)
+                int shift = 0;
+                int bytesToRead = 0;
+                int bytesEncoded = 0;
+                int inputSize = -1;
+
+                ProgressBar progressBar = new ProgressBar()
                 {
-                    byte[] readBuffer = new byte[LookAheadBufferSize + 1];
+                    OffsetY = 0,
+                    TitleMessages = new List<string>() { $"Compressing {inputFileName}", $"Size: {String.Format("{0:0.00}", inputStream.Length / 1024.0)}kB" },
+                    FinishMessages = new List<string>() { $"Compressing to {outputFileName} completed." },
+                    MaxValue = inputStream.Length,
+                    StepsCount = 50
+                };
 
-                    // bytesToRead == 0 means it's the beggining of compression, therefore fully load the buffer.
-                    // Min(shift, LookAheadBufferSize) Makes sure not to exceed the buffer's capacity.
-                    bytesToRead = bytesToRead == 0 ? lookAheadBuffer.Length : Math.Min(shift, lookAheadBuffer.Length);
+                using (Stream outputStream = File.OpenWrite(outputFileName))
+                {
+                    BinaryWriter binaryWriter = new BinaryWriter(outputStream);
 
-                    if (inputSize != 0) inputSize = inputStream.Read(readBuffer, 0, bytesToRead);
+                    while (LookAheadBufferSize > 0)
+                    {
+                        byte[] readBuffer = new byte[LookAheadBufferSize + 1];
 
-                    // Filling the buffers with the amount of bytes processed in previous iteration.
-                    codeBuffer = codeBuffer.Skip(shift).Concat(lookAheadBuffer.Take(shift)).ToArray();
+                        // bytesToRead == 0 means it's the beggining of compression, therefore fully load the buffer.
+                        // Min(shift, LookAheadBufferSize) Makes sure not to exceed the buffer's capacity.
+                        bytesToRead = bytesToRead == 0 ? lookAheadBuffer.Length : Math.Min(shift, lookAheadBuffer.Length);
 
-                    if (shift == 0) Array.Copy(readBuffer, lookAheadBuffer, bytesToRead);
-                    else lookAheadBuffer = lookAheadBuffer.Skip(shift).Concat(readBuffer.Take(bytesToRead)).ToArray();
+                        if (inputSize != 0) inputSize = inputStream.Read(readBuffer, 0, bytesToRead);
 
-                    // Check whether the file had been entirely processed.
-                    if (bytesEncoded >= inputStream.Length) break;
+                        // Filling the buffers with the amount of bytes processed in previous iteration.
+                        codeBuffer = codeBuffer.Skip(shift).Concat(lookAheadBuffer.Take(shift)).ToArray();
 
-                    // Longest found matching sequence between lookahead and code buffers
-                    Sequence repetition = FindRepetition(codeBuffer, lookAheadBuffer);
+                        if (shift == 0) Array.Copy(readBuffer, lookAheadBuffer, bytesToRead);
+                        else lookAheadBuffer = lookAheadBuffer.Skip(shift).Concat(readBuffer.Take(bytesToRead)).ToArray();
 
-                    WriteEncoded(binaryWriter, repetition);
+                        // Longest found matching sequence between lookahead and code buffers
+                        Sequence repetition = FindRepetition(codeBuffer, lookAheadBuffer);
 
-                    shift = repetition.Length + 1;
-                    bytesEncoded += shift;
+                        WriteEncoded(binaryWriter, repetition);
+
+                        shift = repetition.Length + 1;
+                        bytesEncoded += shift;
+
+                        // Check whether the file had been entirely processed.
+                        if (bytesEncoded >= inputStream.Length)
+                        {
+                            progressBar.FinishMessages.Add($"Size: {String.Format("{0:0.00}", outputStream.Length / 1024.0)}kB");
+                            progressBar.Value += shift;
+                            break;
+                        }
+
+                        progressBar.Value += shift;
+                    }
                 }
+
             }
         }
 
@@ -139,11 +162,11 @@ namespace DictionaryCoder.Coder
             int offset = sequence.Offset;
             int length = sequence.Length;
             byte nextChar = sequence.NextChar;
-            Console.WriteLine($"\n({offset},{length}){(char)nextChar}");
+            //Console.WriteLine($"\n({offset},{length}){(char)nextChar}");
 
-            // Saving offset and length to bytearrays of given size in little endian notation
-            byte[] offsetData = FormatToByteArray(OffsetBytesCount, offset);
-            byte[] lengthData = FormatToByteArray(LengthBytesCount, length);
+            // Saving offset and length to bytearrays of previously calculated size
+            byte[] offsetData = IntToByteArray(OffsetBytesCount, offset);
+            byte[] lengthData = IntToByteArray(LengthBytesCount, length);
 
             //writing length information within the same byte as last part of offset
             // if you can save a byte do this
@@ -153,88 +176,108 @@ namespace DictionaryCoder.Coder
         }
 
         /**
-         * Parameters  : bytesCount - count of bytes to wtore data within.
+         * Parameters  : bytesCount - count of bytes to contain data within.
          *             : data - integer value to format to byte array.
-         * Return      : bytearray containing given information within desired size
+         * Return      : bytearray containing given information contained within desired size,
          *               written in little endian notation
+         * Byte arrangement difference: (example for 4 bytes)
+         *               Big endian    : most significant byte at the beggining -> 04 03 02 01
+         *               Little endian : 01 02 03 04 <- most significant byte at the end
          */
-        byte[] FormatToByteArray(int bytesCount, int data)
+        byte[] IntToByteArray(int bytesCount, int data)
         {
             byte[] byteArray = BitConverter.GetBytes(data);
-            if (BitConverter.IsLittleEndian)
-            {
-                byteArray = byteArray.Take(bytesCount).ToArray();
-            }
-            else
-            {
-                byteArray = byteArray.Skip(byteArray.Length - bytesCount).ToArray();
-                Array.Reverse(byteArray);
-            }
+            if (!BitConverter.IsLittleEndian) Array.Reverse(byteArray);
+            byteArray = byteArray.Take(bytesCount).ToArray();
             return byteArray;
         }
 
         /**
         * Decompresses given data using LZ77 algorithm.
+        * Compressed data has a structure of <O,L,C>, where:
+        *           O - Offset of the first character
+        *           L - Length of the repeated sequence
+        *           C - Character following the sentence
         * Parameters : inputStream - file stream to whitch write decompressed data.
         *              outputPath - path to where create and write encoded data to
         */
-        public void DecodeLZ77(Stream inputStream, string outputPath)
+        public void DecodeLZ77(string inputFileName, string outputFileName)
         {
-            int bytesDecoded = 0;
-            int bytesToRead = SizeSum;
-            byte[] readBuffer = new byte[bytesToRead];
-            byte[] codeBuffer = new byte[CodeBufferSize];
-            bool initialize = true;
-            byte[] outputArray = new byte[0];
-
-            Console.WriteLine("DECOOODEEE");
-            using (Stream outputStream = new FileStream(outputPath, FileMode.OpenOrCreate, FileAccess.Write))
+            using (Stream inputStream = new FileStream(inputFileName, FileMode.Open, FileAccess.Read))
             {
-                StreamWriter streamWriter = new StreamWriter(outputStream, Encoding.UTF8);
+                int bytesDecoded = 0;
+                int bytesToRead = SizeSum;
+                byte[] readBuffer = new byte[bytesToRead];
+                byte[] codeBuffer = new byte[CodeBufferSize];
+                byte[] outputArray = new byte[0];
 
-                while (bytesToRead > 0)
+                ProgressBar progressBar = new ProgressBar()
                 {
-                    int bytesRead = inputStream.Read(readBuffer, 0, bytesToRead);
-                    if (bytesRead == 0) break;
-                    if (bytesRead != bytesToRead) Console.WriteLine("Invalid data format!");
+                    OffsetY = Console.CursorTop + 1,
+                    TitleMessages = new List<string>() {
+                        $"Decompressing {inputFileName}", $"Size: {String.Format("{0:0.00}", inputStream.Length / 1024.0)}kB"  },
+                    FinishMessages = new List<string>() {
+                        $"Decompression to {outputFileName} completed." },
+                    MaxValue = inputStream.Length,
+                    StepsCount = 50
+                };
 
-                    byte[] lenBytes = new byte[] { (byte)((readBuffer[^2] & 0xF0) >> 4), 0x00 };
-
-                    // Remove length data from the array
-                    readBuffer[^2] = (byte)(readBuffer[^2] & 0x0F);
-
-                    int offset = BitConverter.ToInt16(readBuffer);
-                    int length = BitConverter.ToInt16(lenBytes);
-                    byte nextChar = readBuffer[^1];
-                    Console.WriteLine($"\n({offset},{length}){(char)nextChar}");
-
-                    // Filling codeBuffer with first byte read if it's the beginning of decoding
-                    if (initialize)
+                using (Stream outputStream = new FileStream(outputFileName, FileMode.OpenOrCreate, FileAccess.Write))
+                {
+                    using (StreamWriter streamWriter = new StreamWriter(outputStream, Encoding.UTF8))
                     {
-                        Array.Fill(codeBuffer, (byte)0x01);
-                        initialize = false;
-                    }
-                    byte[] tempbuf = new byte[codeBuffer.Length];
-                    Array.Copy(codeBuffer, tempbuf, codeBuffer.Length);
+                        while (bytesToRead > 0)
+                        {
+                            int bytesRead = inputStream.Read(readBuffer, 0, bytesToRead);
+                            if (bytesRead == 0) break;
+                            if (bytesRead != bytesToRead) Console.WriteLine("Invalid data format!");
 
-                    byte[] decoded = new byte[length + 1];
-                    for (int i = 0; i < length; i++)
-                    {
-                        decoded[i] = tempbuf[offset + i];
-                        tempbuf = tempbuf.Concat(decoded.Skip(i).Take(1)).ToArray();
-                    }
-                    decoded[^1] = nextChar;
-                    codeBuffer = codeBuffer.Skip(decoded.Length).Concat(decoded).ToArray();
-                    outputArray = outputArray.Concat(decoded).ToArray();
+                            // Extract length information from second byte
+                            byte[] lenBytes = new byte[] { (byte)((readBuffer[^2] & 0xF0) >> 4), 0x00 };
 
-                    bytesDecoded += bytesRead;
-                    if (bytesDecoded >= inputStream.Length) break;
+                            // Remove length data from the array
+                            readBuffer[^2] = (byte)(readBuffer[^2] & 0x0F);
+
+                            int offset = BitConverter.ToInt16(readBuffer);
+                            int length = BitConverter.ToInt16(lenBytes);
+                            byte nextChar = readBuffer[^1];
+                            //Console.WriteLine($"\n({offset},{length}){(char)nextChar}");
+
+                            byte[] decoded = new byte[length + 1];
+                            byte[] tempbuf = new byte[codeBuffer.Length];
+                            Array.Copy(codeBuffer, tempbuf, codeBuffer.Length);
+
+                            for (int i = 0; i < length; i++)
+                            {
+                                decoded[i] = tempbuf[offset + i];
+
+                                // Appending decoded byte to the temporary bufer,
+                                // so data can be continuously read from within lookahead buffer.
+                                tempbuf = tempbuf.Concat(decoded.Skip(i).Take(1)).ToArray();
+                            }
+                            decoded[^1] = nextChar;
+
+                            // Remove unnecessary bytes, append newly decoded ones.
+                            codeBuffer = codeBuffer.Skip(decoded.Length).Concat(decoded).ToArray();
+
+                            // Store decoded information into an array.
+                            outputArray = outputArray.Concat(decoded).ToArray();
+
+                            // Add up length of decoded bytes, check for decompression completion.
+                            bytesDecoded += bytesRead;
+                            if (bytesDecoded >= inputStream.Length)
+                            {
+                                progressBar.FinishMessages.Add($"Size: {String.Format("{0:0.00}", (outputStream.Length + bytesRead) / 1024.0)}kB");
+                                progressBar.Value += bytesRead;
+                                break;
+                            }
+                            progressBar.Value += bytesRead;
+                        }
+
+                        string message = Encoding.UTF8.GetString(outputArray);
+                        streamWriter.WriteLine(message);
+                    };
                 }
-                string message = Encoding.UTF8.GetString(outputArray);
-                streamWriter.WriteLine(message);
-                streamWriter.Close();
-
-                Console.WriteLine(message);
             }
         }
     }
---
+++ DictionaryCoder/Coder/ProgressBar.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Coder
+{
+    class ProgressBar
+    {
+        private ConsoleColor[] colors = new ConsoleColor[] {
+            ConsoleColor.DarkRed, ConsoleColor.Red,
+            ConsoleColor.DarkBlue, ConsoleColor.DarkBlue,
+            ConsoleColor.DarkGreen, ConsoleColor.Green
+        };
+        public int OffsetY { get; set; }
+        public List<string> TitleMessages { get; set; }
+        public List<string> FinishMessages { get; set; }
+        public float MaxValue { get; set; }
+
+        private float _value;
+        public float Value
+        {
+            get { return _value; }
+            set
+            {
+                _value = value;
+                Display();
+            }
+        }
+        public int StepsCount { get; set; }
+
+        public ProgressBar(float maxValue = 100, int stepsCount = 50)
+        {
+            MaxValue = maxValue;
+            _value = 0;
+            StepsCount = stepsCount;
+        }
+
+        public void Display()
+        {
+            List<string> messages = new List<string>();
+            try
+            {
+                int offsetY = OffsetY;
+                string progressBar = "[";
+                for (int step = 1; step <= StepsCount; step++) progressBar += IsCompleted(step, _value) ? '-' : ' ';
+                progressBar += "]";
+
+                messages.AddRange(TitleMessages);
+                messages.Add(progressBar);
+                messages.Add(GetProgressMessage(_value));
+                if (_value >= MaxValue) messages.AddRange(FinishMessages);
+
+                Console.CursorVisible = false;
+
+                foreach (string message in messages)
+                {
+                    if (message == progressBar) Console.ForegroundColor = GetColor(_value);
+                    else Console.SetCursorPosition(GetOffsetToCenter(message), offsetY);
+                    Console.WriteLine(message);
+                    Console.ResetColor();
+                    offsetY++;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.SetCursorPosition(0, OffsetY + messages.Count + 2);
+                Console.WriteLine("Progressbar exception" + e.StackTrace);
+            }
+        }
+
+        private string GetProgressMessage(float value)
+        {
+            return $"{(int)(value / MaxValue * 100)}% Completed";
+        }
+
+        private int GetOffsetToCenter(string text)
+        {
+            int offset = (StepsCount + 2 - text.Length) / 2;
+            return offset > 0 ? offset : 0;
+        }
+
+        private bool IsCompleted(float step, float value)
+        {
+            return value / MaxValue >= step / StepsCount;
+        }
+
+        private ConsoleColor GetColor(float value)
+        {
+            int index = value == 0 ? 0 : (int)Math.Ceiling(value / MaxValue * colors.Length) - 1;
+            index = index > 5 ? 5 : index;
+            return colors[index];
+        }
+    }
+}
